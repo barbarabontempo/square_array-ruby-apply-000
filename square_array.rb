62974c7003c5ba342bb0d93e855984b1c2b1de51
Barbara Bontempo <barbarabontempo94@gmail.com>
2020-02-29 00:31:05 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
-  array = [2,4,6]
   arr = []
-  array.each { |i| arr << i ** 2 }
+  array.each { |b| arr << i ** 2 }
   square_array.each(array)
-  # your code here
 end
+
+  array = [2,4,6]
